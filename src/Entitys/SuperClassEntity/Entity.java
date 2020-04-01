704618091e380abac83c3de5d17cef2244fcfc2f
Yordan-Petrov-git <y.petrov@ue-varna.bg>
2020-04-02 01:34:59 +0300

    added  entity class attributes

--- src/Entitys/SuperClassEntity/Entity.java
+++ src/Entitys/SuperClassEntity/Entity.java
@@ -4,33 +4,47 @@ public class Entity {
 
     private String name;
 
+    private int entityClass;
+
     private double health;
     private double mana;
     private double stamina;
+
+    private double movementSpeed;
+
     private double attackDamage;
     private double abilityPower;
+    private double attackSpeed;
+    private double castingSpeed;
+    private double lifesteal;
+
     private double criticalChance;
+    private double lifestealChance;
+
     private double defence;
     private double block;
+
     private double parryChance;
     private double hitChance;
     private double missChance;
-    private double lifestealChance;
-    private double lifesteal;
+
     private double bleedResist;
     private double slashResist;
     private double punctureResist;
+
     private double magicktResist;
     private double holyResist;
-    private double enemyDarkResist;
+    private double darkResist;
+
     private double fireResist;
-    private double efrostResist;
+    private double frostResist;
     private double shockResist;
+
+    private double souls;
+
     private double hunger;
+
     private boolean isEntityBoss;
-
-
-
 
 
 }
