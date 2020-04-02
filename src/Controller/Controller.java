0cd94f7e664eb2d7ce254b670085341b89885a49
Yordan-Petrov-git <y.petrov@ue-varna.bg>
2020-04-02 15:03:05 +0300

    STRUCTURED CLASSES monster enemy

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -5,6 +5,7 @@ import Models.Entitys.Enemies.Monsters.Adventurer;
 import Models.Entitys.Heroes.Hero;
 
 public class Controller {
+
   public Hero currentHero;
   public Adventurer adventurer;
        
