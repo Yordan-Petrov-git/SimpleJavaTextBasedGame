a44a47f6cb939aae8023d9b80445ad5bf0f5dc5f
Yordan-Petrov-git <y.petrov@ue-varna.bg>
2020-04-02 04:52:53 +0300

    STRUCTURED CLASSES monster enemy

--- src/Models/Entitys/Enemies/Monsters/Goblin.java
+++ src/Models/Entitys/Enemies/Monsters/Goblin.java
@@ -3,7 +3,18 @@ package Models.Entitys.Enemies.Monsters;
 import Models.Entitys.Enemies.SuperClassEnemy.Enemy;
 
 public class Goblin extends Enemy {
-    public Goblin(String name, int entityClass, int entityRace, int entityLevel, double health, double mana, double stamina, double movementSpeed, double attackDamage, double abilityPower, double attackSpeed, double castingSpeed, double lifeSteal, double criticalChance, double lifeStealChance, double defence, double block, double parryChance, double hitChance, double missChance, double bleedResist, double slashResist, double punctureResist, double magickResist, double holyResist, double darkResist, double fireResist, double frostResist, double shockResist, double souls, double hunger, boolean isEntityBoss, double dropAmountExperience, double dropAmountGold, double dropAmountItems) {
-        super(name, entityClass, entityRace, entityLevel, health, mana, stamina, movementSpeed, attackDamage, abilityPower, attackSpeed, castingSpeed, lifeSteal, criticalChance, lifeStealChance, defence, block, parryChance, hitChance, missChance, bleedResist, slashResist, punctureResist, magickResist, holyResist, darkResist, fireResist, frostResist, shockResist, souls, hunger, isEntityBoss, dropAmountExperience, dropAmountGold, dropAmountItems);
+
+    public Goblin(String name, int entityClass, int entityRace, int entityLevel, double health, double mana, double stamina
+            , double movementSpeed, double attackDamage, double abilityPower, double attackSpeed, double castingSpeed
+            , double lifeSteal, double criticalChance, double lifeStealChance, double defence, double block
+            , double parryChance, double hitChance, double missChance, double bleedResist, double slashResist
+            , double punctureResist, double magickResist, double holyResist, double darkResist, double fireResist
+            , double frostResist, double shockResist, double souls, double hunger, boolean isEntityBoss
+            , double dropAmountExperience, double dropAmountGold, double dropAmountItems) {
+
+        super(name, entityClass, entityRace, entityLevel, health, mana, stamina, movementSpeed, attackDamage, abilityPower
+                , attackSpeed, castingSpeed, lifeSteal, criticalChance, lifeStealChance, defence, block, parryChance, hitChance
+                , missChance, bleedResist, slashResist, punctureResist, magickResist, holyResist, darkResist, fireResist
+                , frostResist, shockResist, souls, hunger, isEntityBoss, dropAmountExperience, dropAmountGold, dropAmountItems);
     }
 }
--- src/Models/Items/Weapons/RangedWeapons/LongBow.java
+++ src/Models/Items/Weapons/RangedWeapons/LongBow.java
@@ -1,8 +1,12 @@
 package Models.Items.Weapons.RangedWeapons;
 
-public class LongBow extends RangedWeapons{
+public class LongBow extends RangedWeapons {
 
-    public LongBow(String idWepon, double damage, double weight, double bleedDamage, double slashDamage, double punctureDamage, double magickDamage, double lifeStealDamage, double frostDamage, double shockDamage, double fireDamage, double darkDamage, double range) {
-        super(idWepon, damage, weight, bleedDamage, slashDamage, punctureDamage, magickDamage, lifeStealDamage, frostDamage, shockDamage, fireDamage, darkDamage, range);
+    public LongBow(String idWepon, double damage, double weight, double bleedDamage, double slashDamage
+            , double punctureDamage, double magickDamage, double lifeStealDamage, double frostDamage
+            , double shockDamage, double fireDamage, double darkDamage, double range) {
+
+        super(idWepon, damage, weight, bleedDamage, slashDamage, punctureDamage, magickDamage, lifeStealDamage
+                , frostDamage, shockDamage, fireDamage, darkDamage, range);
     }
 }
