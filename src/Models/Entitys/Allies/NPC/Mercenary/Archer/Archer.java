edce61dd992a0f53c40affe0626e322d70baadfd
Yordan-Petrov-git <y.petrov@ue-varna.bg>
2020-04-02 04:29:28 +0300

    STRUCTURED CLASSES

--- src/Models/Entitys/Allies/NPC/Mercenary/Archer/Archer.java
+++ src/Models/Entitys/Allies/NPC/Mercenary/Archer/Archer.java
@@ -1,4 +1,9 @@
 package Models.Entitys.Allies.NPC.Mercenary.Archer;
 
-public class Archer {
+import Models.Entitys.Allies.NPC.Mercenary.Mercenary;
+
+public class Archer extends Mercenary {
+    public Archer(String name, int entityClass, int entityRace, int entityLevel, double health, double mana, double stamina, double movementSpeed, double attackDamage, double abilityPower, double attackSpeed, double castingSpeed, double lifeSteal, double criticalChance, double lifeStealChance, double defence, double block, double parryChance, double hitChance, double missChance, double bleedResist, double slashResist, double punctureResist, double magickResist, double holyResist, double darkResist, double fireResist, double frostResist, double shockResist, double souls, double hunger, boolean isEntityBoss, double hirePrice) {
+        super(name, entityClass, entityRace, entityLevel, health, mana, stamina, movementSpeed, attackDamage, abilityPower, attackSpeed, castingSpeed, lifeSteal, criticalChance, lifeStealChance, defence, block, parryChance, hitChance, missChance, bleedResist, slashResist, punctureResist, magickResist, holyResist, darkResist, fireResist, frostResist, shockResist, souls, hunger, isEntityBoss, hirePrice);
+    }
 }
